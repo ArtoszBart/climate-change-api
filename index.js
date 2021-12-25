847fcd1a13009bb98b6f2710a60ba0f9b6d39cf8
Bartosz Art <bartosz.art@gmail.com>
2021-12-25 23:37:23 +0100

    Modified index.js to handle /news route

--- index.js
+++ index.js
@@ -5,8 +5,30 @@ const cheerio = require('cheerio');
 
 const app = express();
 
+const articles = [];
+
+// HOME ROUTE
 app.get('/', (req, res) => {
 	res.json('Welcome to my Climate Change News API')
+});
+
+// NEWS ROUTE
+app.get('/news', (req, res) => {
+	axios.get('https://www.theguardian.com/environment/climate-crisis')
+		.then((response) => {
+			const html = response.data;
+			const $ = cheerio.load(html);
+
+			$('a:contains("climate")', html).each(function () { 	// any <a> that contain anything with "climate"
+				const title = $(this).text(); 					// <a> text
+				const url = $(this).attr('href');				// <a> href attribute
+				articles.push({
+					title,
+					url
+				});
+			});
+			res.json(articles);
+		}).catch((err) => console.log(err));
 });
 
 app.listen(PORT, () => console.log(`server running on PORT ${PORT}`));
