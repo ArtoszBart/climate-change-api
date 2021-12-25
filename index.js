2f40fea8c17bf8ea0c7853ad4548e2e8217df314
Bartosz Art <bartosz.art@gmail.com>
2021-12-26 00:03:02 +0100

    Modified index.js

--- index.js
+++ index.js
@@ -2,10 +2,47 @@ const PORT = 7000;
 const express = require('express');
 const axios = require('axios');
 const cheerio = require('cheerio');
+const { response } = require('express');
 
 const app = express();
 
+// newspapers to be searched
+const newspapers = [
+	{
+		name: 'theguardian',
+		address: 'https://www.theguardian.com/environment/climate-crisis',
+		base: ''
+	},
+	{
+		name: 'thetimes',
+		address: 'https://www.thetimes.co.uk/environment/climate-change',
+		base: ''
+	},
+	{
+		name: 'telegraph',
+		address: 'https://www.telegraph.co.uk/climate-change',
+		base: 'https://www.telegraph.co.uk'
+	}
+];
 const articles = [];
+
+newspapers.forEach(newspaper => {
+	axios.get(newspaper.address)
+		.then(response => {
+			const html = response.data;
+			const $ = cheerio.load(html);
+
+			$('a:contains("climate")', html).each(function () { // any <a> that contain anything with "climate"
+				const title = $(this).text(); 					// <a> text
+				const url = $(this).attr('href');				// <a> href attribute
+				articles.push({
+					title,
+					url: newspaper.base + url,
+					source: newspaper.name
+				});
+			});
+		});
+});
 
 // HOME ROUTE
 app.get('/', (req, res) => {
@@ -14,21 +51,7 @@ app.get('/', (req, res) => {
 
 // NEWS ROUTE
 app.get('/news', (req, res) => {
-	axios.get('https://www.theguardian.com/environment/climate-crisis')
-		.then((response) => {
-			const html = response.data;
-			const $ = cheerio.load(html);
-
-			$('a:contains("climate")', html).each(function () { 	// any <a> that contain anything with "climate"
-				const title = $(this).text(); 					// <a> text
-				const url = $(this).attr('href');				// <a> href attribute
-				articles.push({
-					title,
-					url
-				});
-			});
-			res.json(articles);
-		}).catch((err) => console.log(err));
+	res.json(articles);
 });
 
 app.listen(PORT, () => console.log(`server running on PORT ${PORT}`));
