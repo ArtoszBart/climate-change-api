e222082ed266a71c80affd9eb9ac4e045f925b17
Bartosz Art <bartosz.art@gmail.com>
2021-12-26 01:22:24 +0100

    Moved source of newspapers from index.js to helpers/newspsperSource.js

--- index.js
+++ index.js
@@ -2,78 +2,12 @@ const PORT = process.env.PORT || 7000;
 const express = require('express');
 const axios = require('axios');
 const cheerio = require('cheerio');
-const { response } = require('express');
+const newspaperSource = require('./helpers/newspaperSource.js');
 
 const app = express();
 
 // newspapers to be searched
-const newspapers = [
-	{
-		name: 'cityam',
-		address: 'https://www.cityam.com/london-must-become-a-world-leader-on-climate-change-action/',
-		base: ''
-	},
-	{
-		name: 'thetimes',
-		address: 'https://www.thetimes.co.uk/environment/climate-change',
-		base: ''
-	},
-	{
-		name: 'theguardian',
-		address: 'https://www.theguardian.com/environment/climate-crisis',
-		base: '',
-	},
-	{
-		name: 'telegraph',
-		address: 'https://www.telegraph.co.uk/climate-change',
-		base: 'https://www.telegraph.co.uk',
-	},
-	{
-		name: 'nyt',
-		address: 'https://www.nytimes.com/international/section/climate',
-		base: 'https://www.nytimes.com',
-	},
-	{
-		name: 'latimes',
-		address: 'https://www.latimes.com/environment',
-		base: '',
-	},
-	{
-		name: 'smh',
-		address: 'https://www.smh.com.au/environment/climate-change',
-		base: 'https://www.smh.com.au',
-	},
-	{
-		name: 'un',
-		address: 'https://www.un.org/climatechange',
-		base: '',
-	},
-	{
-		name: 'bbc',
-		address: 'https://www.bbc.co.uk/news/science_and_environment',
-		base: 'https://www.bbc.co.uk',
-	},
-	{
-		name: 'es',
-		address: 'https://www.standard.co.uk/topic/climate-change',
-		base: 'https://www.standard.co.uk'
-	},
-	{
-		name: 'sun',
-		address: 'https://www.thesun.co.uk/topic/climate-change-environment/',
-		base: ''
-	},
-	{
-		name: 'dm',
-		address: 'https://www.dailymail.co.uk/news/climate_change_global_warming/index.html',
-		base: ''
-	},
-	{
-		name: 'nyp',
-		address: 'https://nypost.com/tag/climate-change/',
-		base: ''
-	}
-];
+const newspapers = newspaperSource.getNewspapers();
 const articles = [];
 
 newspapers.forEach(newspaper => {
@@ -93,12 +27,12 @@ newspapers.forEach(newspaper => {
 					});
 				}
 			});
-		});
+		}).catch(err => console.log(err));
 });
 
 // HOME ROUTE
 app.get('/', (req, res) => {
-	res.json('Welcome to my Climate Change News API')
+	res.json('Welcome to my Climate Change News API');
 });
 
 // NEWS ROUTE
@@ -123,7 +57,7 @@ app.get('/news/:newspaperId', (req, res) => {
 				const url = $(this).attr('href');
 				newspaperArticles.push({
 					title,
-					url: newspaperBase + url,
+					url: (url.includes('https://twitter.com/') ? '' : newspaperBase) + url,
 					source: newspaperId
 				});
 			});
